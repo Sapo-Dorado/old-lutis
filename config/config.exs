5391afefb60c8551281bf5463b967979580bee16
Nicholas Brown <countdooku.steak@gmail.com>
2020-10-27 20:14:13 -0700

    actually fixed host

--- config/config.exs
+++ config/config.exs
@@ -12,7 +12,7 @@ config :lutis,
 
 # Configures the endpoint
 config :lutis, LutisWeb.Endpoint,
-  url: [host: "lutis.io"],
+  url: [host: "localhost"],
   secret_key_base: "RErBhypdWc7asOmCduHhxBbjmsxW3L32aHC/jtVSi/z3kDilRcF4DEiQWR+sHzMy",
   render_errors: [view: LutisWeb.ErrorView, accepts: ~w(html json), layout: false],
   pubsub_server: Lutis.PubSub,
